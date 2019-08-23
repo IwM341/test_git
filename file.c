4321d4da64513d03ad64ab84d99856a94a2b6046
Artem <artem_tovstun@mail.ru>
2019-08-23 16:20:09 +0300

    v2

---
+++ file.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void test_func(void){
+
+		printf("this is a est func!");
+}
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+void test_func(void);
 int main(void)
 {
 	printf("hello world!");
 	printf("by");
+	test_func();
 	return 0;
 }
